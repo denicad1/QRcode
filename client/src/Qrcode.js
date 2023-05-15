c8aa45b5cc72144d08d82a9e95a8c7eaea57ec85
anthony <denicad@msn.com>
2023-05-15 15:44:47 -0400

    some styling added

--- client/src/Qrcode.js
+++ client/src/Qrcode.js
@@ -7,13 +7,11 @@ function Qrcode (props) {
     const [code,setCode]=useState();
     let [text,setText]=useState();
         const info=(e)=>{
-            console.log(e.target.value);
             setText(e.target.value);
         }
     
         async function onSubmit(e){
             e.preventDefault();
-            console.log('running');
             let phrase={
                 message:text
             }
@@ -30,16 +28,33 @@ function Qrcode (props) {
             });
         }
        
+        function iterateDots(){
+            var el = document.getElementById("dots");
+            var dotsStr = el.innerHTML;
+            console.log(dotsStr);
+            var dotsLen = dotsStr.length;
+            console.log(dotsLen);
+        
+            var maxDots = 3;
+            el.innerHTML = (dotsLen < maxDots ? dotsStr + '.' : dotsStr='');
+        }
+        
+        function startLoading(){
+            const intervalMs = 1000;
+        
+            let interval = setInterval(iterateDots, intervalMs);
+        }  
 
     return (
     <div>
-       
-       <form onSubmit={onSubmit} method='POST'>
-           <input type="text" name="message" id="message" onChange={info} placeholder="enter text here"/>
-           <button type='submit'>Submit text for qrCode</button>
-       </form>
-      {code?<img src={code}></img>:<div>Waiting for input</div>}
-       
+        <div className='margin-bottom'>
+            <span className='title'>QR Code Generator</span>
+        </div>
+        {code?<img src={code} className='margin-bottom'></img>:<div className='margin-bottom'>Waiting for input<span id='dots' onLoad={startLoading()}></span></div>}
+       <form onSubmit={onSubmit} method='POST' className='flex-center flex-col'>
+           <input type="text" className='input' name="message" id="message" onChange={info} placeholder=" Enter text here"/>
+           <button type='submit' className='submit button-text'>Submit text for qrCode</button>
+       </form>       
     </div>);
 }
 
